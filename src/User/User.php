8f3dc3e435744b4094f79fe2afbe73edb69ae2ed
Anik Goel <anikgoel19@gmail.com>
2016-07-04 16:28:21 +0530

    support for user service added currently onyl get user function added

---
+++ src/User/User.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace JiraRestApi\User;
+
+/**
+ * Description of User
+ *
+ * @author Anik
+ */
+class User implements \JsonSerializable {
+
+    /**
+     * uri which was hit
+     * @var string 
+     */
+    public $self;
+
+    /**
+     * @var string 
+     */
+    public $key;
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var string 
+     */
+    public $emailAddress;
+
+    /**
+     * @var object
+     */
+    public $avatarUrls;
+
+    /**
+     * @var string
+     */
+    public $displayName;
+
+    /**
+     * @var boolean
+     */
+    public $active;
+
+    /**
+     * @var string
+     */
+    public $timeZone;
+
+    /**
+     * @var array  "#/definitions/simple-list-wrapper"
+     */
+    public $groups;
+
+    /**
+     * @var array  "#/definitions/simple-list-wrapper"
+     */
+    public $applicationRoles;
+
+    /**
+     * @var string
+     */
+    public $expand;
+
+    public function jsonSerialize() {
+        return array_filter(get_object_vars($this));
+    }
+
+}
---
+++ src/User/UserService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace JiraRestApi\User;
+
+/**
+ * Class to perform all user related queries
+ *
+ * @author Anik
+ */
+class UserService extends \JiraRestApi\JiraClient {
+
+    private $uri = '/user';
+
+    /**
+     * Function to get user
+     * @param array $paramArray
+     * Possible values for $paramArray 'username', 'key'
+     * @return User class
+     */
+    public function get($paramArray = []) {
+        $queryParam = '?' . http_build_query($paramArray);
+
+        $ret = $this->exec($this->uri . $queryParam, null);
+
+        $this->log->addInfo("Result=\n" . $ret);
+
+        return $issue = $this->json_mapper->map(
+                json_decode($ret), new User()
+        );
+    }
+
+}
