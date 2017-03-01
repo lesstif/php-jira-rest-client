ffda8fed4775a3cdf1bbea4e6be893406b78de3f
KwangSeob Jeong <lesstif@gmail.com>
2017-03-01 18:39:41 +0900

    fixed #64

--- src/Issue/Comment.php
+++ src/Issue/Comment.php
@@ -66,14 +66,26 @@ class Comment implements \JsonSerializable
         return $this;
     }
 
-    public function setVisibility($type, $vvalue)
+    /**
+     * @param Visibility $type
+     * @param null $value
+     * @return $this
+     */
+    public function setVisibility($type, $value = null)
     {
         if (is_null($this->visibility)) {
             $this->visibility = new Visibility();
         }
 
-        $this->visibility->setType($type);
-        $this->visibility->setValue($vvalue);
+        if (is_array($type)) {
+            $this->visibility->setType($type['type']);
+            $this->visibility->setValue($type['value']);
+        } elseif ($type instanceof Visibility) {
+            $this->visibility = $type;
+        } else {
+            $this->visibility->setType($type);
+            $this->visibility->setValue($value);
+        }
 
         return $this;
     }
