fac68949bd35a794f8c0e540f4bbe16c21e3f5b0
Arnon Meshoulam <arnon.meshoulam@intel.com>
2018-05-24 14:09:09 +0300

    Adding basic support for Sprin

---
+++ src/sprint/Sprint.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: meshulam
+ * Date: 23/09/2017
+ * Time: 14:17
+ */
+
+namespace JiraRestApi\Sprint;
+
+use JiraRestApi\ClassSerialize;
+use JiraRestApi\JiraException;
+use JiraRestApi\Sprint\SprintService;
+
+
+class Sprint implements \JsonSerializable
+{
+    /**
+     * return only if Project query by key(not id).
+     *
+     * @var string
+     */
+    public $expand;
+
+    /* @var string */
+    public $self;
+
+    /* @var string */
+    public $id;
+
+
+    /* @var string*/
+    public $name;
+
+    /* @var string */
+    //public $type;
+
+    /* @var string */
+    public $state;
+
+    /* @var string */
+    public $startDate;
+
+    /* @var string */
+    public $endDate;
+
+    /* @var string */
+    public $originalBoardiD;
+
+    public function jsonSerialize()
+    {
+        return array_filter(get_object_vars($this));
+    }
+
+    public function setName ($sprintName) {
+        $this->name = $sprintName;
+        return $sprintName;
+    }
+    public function getName () {
+        return $this->name;
+    }
+
+}
