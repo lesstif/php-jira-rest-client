86f05a972aba69305a984e8f141240ee9988c298
yaronkhazai <yaronkh@gmail.com>
2020-05-20 15:17:14 +0300

    Update CommentV3.php

--- src/Issue/CommentV3.php
+++ src/Issue/CommentV3.php
@@ -4,16 +4,14 @@ namespace JiraRestApi\Issue;
 
 class CommentV3 extends Comment
 {
-    
 
-    
     /** @var \JiraRestApi\Issue\DescriptionV3|null */
     public $body;
 
     
     public function setBody($body)
     {
-		$this->body = new DescriptionV3();
+        $this->body = new DescriptionV3();
         $this->body->addDescriptionContent('paragraph', $body);
 
         return $this;
