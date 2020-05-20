b854e314ec084a2d4d837f89c89090bb42fd94cc
yaronkhazai <yaronkh@gmail.com>
2020-05-20 14:00:26 +0300

    added support for comment in API V3

---
+++ src/Issue/CommentV3.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace JiraRestApi\Issue;
+
+class CommentV3 extends Comment
+{
+    
+
+    
+    /** @var \JiraRestApi\Issue\DescriptionV3|null */
+    public $body;
+
+    
+    public function setBody($body)
+    {
+		$this->body = new DescriptionV3();
+        $this->body->addDescriptionContent('paragraph', $body);
+
+        return $this;
+    }
+
+    
+}
