2828819cd9e22cbe30a37ab5105746afeafc18ab
Christian Ziermann <christian.ziermann@plusserver.com>
2020-11-18 14:35:57 +0100

    Add new Services
    Add StatusCategoryService
    Add IssueTypeService

---
+++ src/IssueType/IssueType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace JiraRestApi\IssueType;
+
+class IssueType implements \JsonSerializable
+{
+
+	/** @var string */
+	public $self;
+
+	/** @var int */
+	public $id;
+
+	/** @var string */
+	public $description;
+
+	/** @var string */
+	public $iconUrl;
+
+	/** @var string */
+	public $name;
+
+	/** @var boolean */
+	public $subtask;
+
+	/** @var integer */
+	public $avatarId;
+
+	public function jsonSerialize()
+	{
+		return array_filter(get_object_vars($this));
+	}
+}
