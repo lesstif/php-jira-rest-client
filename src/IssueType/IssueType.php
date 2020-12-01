06a205563843a6bbe5a44328f36f7750006161f2
Christian Ziermann <christian.ziermann@plusserver.com>
2020-12-01 15:29:27 +0100

    Fix replace Tabs with Spaces

--- src/IssueType/IssueType.php
+++ src/IssueType/IssueType.php
@@ -5,29 +5,29 @@ namespace JiraRestApi\IssueType;
 class IssueType implements \JsonSerializable
 {
 
-	/** @var string */
-	public $self;
+    /** @var string */
+    public $self;
 
-	/** @var int */
-	public $id;
+    /** @var int */
+    public $id;
 
-	/** @var string */
-	public $description;
+    /** @var string */
+    public $description;
 
-	/** @var string */
-	public $iconUrl;
+    /** @var string */
+    public $iconUrl;
 
-	/** @var string */
-	public $name;
+    /** @var string */
+    public $name;
 
-	/** @var boolean */
-	public $subtask;
+    /** @var boolean */
+    public $subtask;
 
-	/** @var integer */
-	public $avatarId;
+    /** @var integer */
+    public $avatarId;
 
-	public function jsonSerialize()
-	{
-		return array_filter(get_object_vars($this));
-	}
+    public function jsonSerialize()
+    {
+        return array_filter(get_object_vars($this));
+    }
 }
