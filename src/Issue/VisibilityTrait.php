0d16550f8086caf9edbf5febc20d85f25dee5ca1
Alex P <alex.pantec@gmail.com>
2021-01-26 11:52:25 +0200

    Increase PHPStan level to 5

--- src/Issue/VisibilityTrait.php
+++ src/Issue/VisibilityTrait.php
@@ -5,8 +5,8 @@ namespace JiraRestApi\Issue;
 trait VisibilityTrait
 {
     /**
-     * @param Visibility $type
-     * @param null       $value
+     * @param Visibility  $type
+     * @param mixed|null  $value
      *
      * @return $this
      */
@@ -21,7 +21,7 @@ trait VisibilityTrait
             $this->visibility->setValue($type['value']);
         } elseif ($type instanceof Visibility) {
             $this->visibility = $type;
-        } else {
+        } else { // @phpstan-ignore-line We cannot fix phpdoc to make this "reachable" because of JsonMapper
             $this->visibility->setType($type);
             $this->visibility->setValue($value);
         }
