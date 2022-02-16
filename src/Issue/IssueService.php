d4075bf67e539388919a49948706724c1d5ba508
Markus Staab <maggus.staab@googlemail.com>
2022-02-16 09:32:00 +0100

    typo

--- src/Issue/IssueService.php
+++ src/Issue/IssueService.php
@@ -1006,7 +1006,7 @@ class IssueService extends \JiraRestApi\JiraClient
     }
 
     /**
-     * Get a remote issue links on the issue.
+     * Get a remote issue link on the issue.
      *
      * @param string|int $issueIdOrKey Issue id Or Key
      *
