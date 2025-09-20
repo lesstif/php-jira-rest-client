18f2ab931ec5bd1f652e8ecfcf4f372765239f5b
Ján Hamrák <hamrak@kcorp.sk>
2025-09-20 06:49:20 +0200

    Support netresearch/jsonmapper ^5.0 (#551)
    * Support netresearch/jsonmapper ^5.0
    
    * codefactor fix

--- src/JiraClient.php
+++ src/JiraClient.php
@@ -73,6 +73,13 @@ class JiraClient
 
         $this->json_mapper = new \JsonMapper();
 
+        // Adjust settings for JsonMapper v5.0 BC
+        if (property_exists($this->json_mapper, 'bStrictNullTypesInArrays')) {
+            $this->json_mapper->bStrictNullTypesInArrays = false; // if you want to allow nulls in arrays
+        }
+        $this->json_mapper->bStrictNullTypes = false; // if you want to allow nulls
+        $this->json_mapper->bStrictObjectTypeChecking = false; // if you want to disable strict type checking
+
         // Fix "\JiraRestApi\JsonMapperHelper::class" syntax error, unexpected 'class' (T_CLASS), expecting identifier (T_STRING) or variable (T_VARIABLE) or '{' or '$'
         $this->json_mapper->undefinedPropertyHandler = [new \JiraRestApi\JsonMapperHelper(), 'setUndefinedProperty'];
 
@@ -198,31 +205,7 @@ class JiraClient
             $this->log->info("Curl $custom_request: $url JsonData=".json_encode($post_data, JSON_UNESCAPED_UNICODE));
         }
 
-        curl_reset($this->curl);
-        $ch = $this->curl;
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->configuration->getTimeout());
-        curl_setopt($ch, CURLOPT_TIMEOUT, $this->configuration->getTimeout());
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_URL, $url);
-
-        // post_data
-        if (!is_null($post_data)) {
-            // PUT REQUEST
-            if (!is_null($custom_request) && $custom_request == 'PUT') {
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-            }
-            if (!is_null($custom_request) && $custom_request == 'DELETE') {
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
-            } else {
-                curl_setopt($ch, CURLOPT_POST, true);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-            }
-        } else {
-            if (!is_null($custom_request) && $custom_request == 'DELETE') {
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
-            }
-        }
+        $ch = $this->prepareCurlRequest($url, $post_data, $custom_request);
 
         // save HTTP Headers
         $curl_http_headers = [
@@ -619,6 +602,37 @@ class JiraClient
         if ($this->getConfiguration()->getProxyType()) {
             curl_setopt($ch, CURLOPT_PROXYTYPE, $this->getConfiguration()->getProxyType());
         }
+    }
+
+    private function prepareCurlRequest(string $url, array|string|null $post_data = null, ?string $custom_request = null)
+    {
+        curl_reset($this->curl);
+        $ch = $this->curl;
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->configuration->getTimeout());
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->configuration->getTimeout());
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        // post_data
+        if (!is_null($post_data)) {
+            // PUT REQUEST
+            if (!is_null($custom_request) && $custom_request == 'PUT') {
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+            }
+            if (!is_null($custom_request) && $custom_request == 'DELETE') {
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+            } else {
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+            }
+        } else {
+            if (!is_null($custom_request) && $custom_request == 'DELETE') {
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+            }
+        }
+
+        return $ch;
     }
 
     /**
