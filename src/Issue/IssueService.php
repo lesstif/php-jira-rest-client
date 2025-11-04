lesstif/php-jira-rest-client

commit 2e79233627ec666dc1693576ff3a82a42b542882

fix deprecated search to search/jql (#554)
Co-authored-by: Tuen Lee <tuen@collectonline.eu>
Co-authored-by: KwangSeob Jeong <lesstif@gmail.com>

src/Issue/IssueService.php
@@ -534,25 +534,26 @@ class IssueService extends \JiraRestApi\JiraClient
      *
      * @return IssueSearchResult
      */
-    public function search(string $jql, int $startAt = 0, int $maxResults = 15, array $fields = [], array $expand = [], bool $validateQuery = true): IssueSearchResult
+    public function search(string $jql, string $nextPageToken = '', int $maxResults = 50, array $fields = [], string $expand = '', array $reconcileIssues = []): IssueBulkResult
     {
-        $data = json_encode([
-            'jql'           => $jql,
-            'startAt'       => $startAt,
-            'maxResults'    => $maxResults,
-            'fields'        => $fields,
-            'expand'        => $expand,
-            'validateQuery' => $validateQuery,
-        ]);
+        $data = [
+            'jql'             => $jql,
+            'maxResults'      => $maxResults,
+            'fields'          => $fields,
+            'expand'          => $expand,
+            'reconcileIssues' => $reconcileIssues,
+        ];
 
-        $ret = $this->exec('search', $data, 'POST');
+        if ($nextPageToken) {
+            $data['nextPageToken'] = $nextPageToken;
+        }
+
+        $ret = $this->exec('search//jql', json_encode($data), 'POST');
         $json = json_decode($ret);
-
-        $result = null;
 
         $result = $this->json_mapper->map(
             $json,
-            new IssueSearchResult()
+            new IssueBulkResult()
         );
 
         return $result;

src/Issue/IssueBulkResult.php
@@ -1,0 +1,82 @@
+<?php
+
+namespace JiraRestApi\Issue;
+
+/**
+ * Issue search result.
+ */
+class IssueBulkResult
+{
+    /**
+     * @var string
+     */
+    public $expand;
+
+    /**
+     * @var \JiraRestApi\Issue\Issue[]
+     */
+    public $issues;
+
+    /**
+     * @var array
+     */
+    public $issueErrors;
+
+    /**
+     * @return array
+     */
+    public function getIssueErrors()
+    {
+        return $this->issueErrors;
+    }
+
+    /**
+     * @param array $issueErrors
+     */
+    public function setIssueErrors($issueErrors)
+    {
+        $this->issueErrors = $issueErrors;
+    }
+
+    /**
+     * @return Issue[]
+     */
+    public function getIssues()
+    {
+        return $this->issues;
+    }
+
+    /**
+     * @param Issue[] $issues
+     */
+    public function setIssues($issues)
+    {
+        $this->issues = $issues;
+    }
+
+    /**
+     * @param int $ndx
+     *
+     * @return Issue
+     */
+    public function getIssue($ndx)
+    {
+        return $this->issues[$ndx];
+    }
+
+    /**
+     * @return string
+     */
+    public function getExpand()
+    {
+        return $this->expand;
+    }
+
+    /**
+     * @param string $expand
+     */
+    public function setExpand($expand)
+    {
+        $this->expand = $expand;
+    }
+}

src/Issue/IssueSearchResult.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Created by PhpStorm.
  * User: keanor
@@ -14,9 +13,8 @@ namespace JiraRestApi\Issue;
  */
 class IssueSearchResult
 {
-    /**
-     * @var string
-     */
+    public ?string $nextPageToken = null;
+
     public ?string $expand = null;
 
     /**
@@ -42,49 +40,17 @@ class IssueSearchResult
     /**
      * @return int
      */
-    public function getStartAt()
+    public function getNextPageToken()
     {
-        return $this->startAt;
+        return $this->nextPageToken;
     }
 
     /**
-     * @param int $startAt
+     * @param string $nextPageToken
      */
-    public function setStartAt($startAt)
+    public function setNextPageToken($nextPageToken)
     {
-        $this->startAt = $startAt;
-    }
-
-    /**
-     * @return int
-     */
-    public function getMaxResults()
-    {
-        return $this->maxResults;
-    }
-
-    /**
-     * @param int $maxResults
-     */
-    public function setMaxResults($maxResults)
-    {
-        $this->maxResults = $maxResults;
-    }
-
-    /**
-     * @return int
-     */
-    public function getTotal()
-    {
-        return $this->total;
-    }
-
-    /**
-     * @param int $total
-     */
-    public function setTotal($total)
-    {
-        $this->total = $total;
+        $this->nextPageToken = $nextPageToken;
     }
 
     /**
@@ -116,7 +82,7 @@ class IssueSearchResult
     /**
      * @return ?string
      */
-    public function getExpand()
+    public function getExpand() : ?$string
     {
         return $this->expand;
     }

commit 0388941bd4c5e3b77fed631217051f766fc38630

fix: wrong parameter annotation in IssueService::search
2e79233 introduced typed parameters, since then the annotation is wrong

annotation does not add further value, so removing them

src/Issue/IssueService.php
@@ -522,13 +522,6 @@ class IssueService extends \JiraRestApi\JiraClient
     /**
      * Search issues.
      *
-     * @param string $jql
-     * @param int    $startAt
-     * @param int    $maxResults
-     * @param array  $fields
-     * @param array  $expand
-     * @param bool   $validateQuery
-     *
      * @throws \JsonMapper_Exception
      * @throws JiraException
      *